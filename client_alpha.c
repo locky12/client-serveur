fdc85622e40888dc00e5255e2c538617958e2a17
Locky12 <francois.letterle@gmail.com>
2017-11-18 14:26:16 +0100

    derniere version

---
+++ client_alpha.c
@@ -0,0 +1,122 @@
+//test git
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+/********************
+ *	Client.c    *
+ ********************/
+
+#define TAILLE_SAISIE 128
+
+ void saisie(char *buffer,char *nom){
+     fgets(buffer,TAILLE_SAISIE,stdin);
+     strcat(nom,buffer);
+ }
+int main (int argc, char *argv[])
+{
+    int socket_connection;
+    struct sockaddr_in adresse_serveur;
+    char buffer_addresse[500];
+    ssize_t taille_envoyee;
+    char buffer_message[128];
+    ssize_t taille_recue;
+    fd_set rd;
+    char buffer_nom[150];
+    char espace[4] = " : ";
+
+
+
+    //
+    char buffer_saisie[TAILLE_SAISIE]; /* Buffer de saisie */
+    memset(buffer_saisie, '\0', sizeof(TAILLE_SAISIE)); /* Initialisation du buffer */
+    /*
+       if (argc != 2)
+       {
+       fprintf(stderr,"Donnez un argument a envoyer \n");
+       exit(-1);
+       }
+       if (strlen( argv[1] ) >= 499 )
+       {
+       fprintf(stderr,"Votre arguments est trop longs \n");
+       exit(-1);
+       }
+       */
+    socket_connection = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socket_connection == -1)
+    {
+	perror( "socket()" );
+	exit(-1);
+    }
+    memset(&adresse_serveur, 0,sizeof(adresse_serveur));
+    adresse_serveur.sin_family = AF_INET;
+
+    adresse_serveur.sin_port = htons (52000);
+    inet_pton (AF_INET, "127.0.0.1", &(adresse_serveur.sin_addr.s_addr));
+
+    if ( connect(
+		socket_connection
+		,(const struct sockaddr *) &adresse_serveur
+		,sizeof( adresse_serveur)
+		) == -1 )
+    {
+	perror("connect()");
+	exit(-1);
+    }
+
+
+    printf( "Connection a %s : %d reussie \n"
+	    ,inet_ntop(
+		AF_INET
+		,&(adresse_serveur.sin_addr)
+		,buffer_addresse
+		,128)
+	    , adresse_serveur.sin_port);
+      printf("Pseudo : " );
+      fgets(buffer_nom,150,stdin );
+      buffer_nom[strlen(buffer_nom)-1]= '\0';
+      strcat(buffer_nom,espace);
+
+
+
+while(1)
+{
+    /* Saisie */
+    FD_ZERO(&rd);
+    FD_SET(STDIN_FILENO,&rd);
+    FD_SET(socket_connection,&rd);
+    select(socket_connection + 1,&rd,0,0,0);
+    if (FD_ISSET(STDIN_FILENO,&rd)){
+    saisie(buffer_saisie,buffer_nom);
+
+
+    taille_envoyee = send( socket_connection, buffer_nom, strlen(buffer_nom), 0);
+    if (taille_envoyee == -1)
+    {
+	perror("send()");
+	exit(-1);
+    }
+
+}
+    //printf("message envoye\n");
+if (FD_ISSET(socket_connection,&rd)) {
+
+
+  	taille_recue = recv( socket_connection, buffer_message, 127, 0);
+
+  	if ( taille_recue == -1 ){
+  	    perror ("recv()" );
+  	    exit( -1 );
+  	}
+    printf(  " %s :", buffer_message );
+    }
+  }
+    close(socket_connection);
+    return 0;
+}
--- serveur_alpha1.c
+++ serveur_alpha1.c
@@ -1,227 +1,254 @@
 
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <unistd.h>
-    #include <sys/types.h>
-    #include <sys/socket.h>
-    #include <netinet/in.h>
-    #include <string.h>
-    #include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
 
-    int lire_client(int socket_client, char *buffer_message)
-    {
+int lire_client(int socket_client, char *buffer_message)
+{
 
 
-      ssize_t		taille_recue;
-      memset( buffer_message, 0, sizeof( buffer_message ) );
-      taille_recue = recv( socket_client, buffer_message, 128, 0);
+    ssize_t		taille_recue;
+    memset( buffer_message, 0, sizeof( buffer_message ) );
+    taille_recue = recv( socket_client, buffer_message, 128, 0);
 
-      if ( taille_recue == -1 ){
-          perror ("recv()" );
-          exit( -1 );
-      }
+    if ( taille_recue == -1 ){
+        perror ("recv()" );
+        exit( -1 );
     }
-    void envoyer_client(int socket_client,char *buffer_message)
+}
+void envoyer_client(int socket_client,char *buffer_message)
+{
+    int taille_envoyee;
+    taille_envoyee = send( socket_client, buffer_message, strlen(buffer_message), 0);
+    if (taille_envoyee == -1)
     {
-      int taille_envoyee;
-      taille_envoyee = send( socket_client, buffer_message, strlen(buffer_message), 0);
-      if (taille_envoyee == -1)
-      {
         perror("send()");
         exit(-1);
-      }
     }
-    void renvoyer_client(int *client,char *buffer,int n)
+}
+void lire_et_renvoyer_client(int *client,int client_parle ,char *buffer,int n)
+{
+
+    char		buffer_message[128];
+    int i;
+    ssize_t	 taille_recue;
+    printf(  "Message reçu (taille %ld): \n[%s]\n"
+    , taille_recue
+    , buffer_message );
+    taille_recue = lire_client(client[i],buffer);
+    for ( i = 0; i < n; i++)
     {
-      char		buffer_message[128];
-      int i;
-      for ( i = 0; i < n; i++)
+      if (client[i] != client_parle)
       {
-          envoyer_client(client[i],buffer);
-      }
+      envoyer_client(client[i],buffer);
     }
-
-
-    int connection ()
-    {
-
-      int			socket_ecoute;			/* Socket d'écoute */
-      struct sockaddr_in	adresse_ecoute;			/* Adr locale ou le server ecoute */
-      socklen_t		taille;				/* Taille pour accept() */
-
-
-
-      puts(" -- Server lancé -- \n");
-      /* Creation de la socket; AF_INET	->  IPv4; SOCK_STREAM	->  TPC	*/
-      if( ( socket_ecoute = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1){
-      perror("socket()");
-      exit(-1);
-      }
-
-      /* Initialisation de la structure qui contient l'adresse écoutée.
-       * La fonction memset() remplit une zone avec ce qu'on veut (ici, 0) */
-      memset(&adresse_ecoute, 0, sizeof(adresse_ecoute));
-      adresse_ecoute.sin_family = AF_INET; /* IPv4 */
-
-      /* Le server écoute sur le port 52000, choisi au hasard
-       * Attention, il faut envoyer les 2 octets dans le bon ordre -> htons() */
-      adresse_ecoute.sin_port = htons( 52000 ); /* Pour récupérer un nombre converti: utilser ntohs */
-
-      /* Petit bricolage pour l'adresse: je mets juste 0.
-       * Donc 0x00000000, et ça dans n'importe quel ordre c'est l'IP 0.0.0.0
-       * 0.0.0.0 = "écoute sur n'importe quelle IP de la machine"
-       * Comment ça "N'importe quelle IP de la machine ?????		 */
-      adresse_ecoute.sin_addr.s_addr = 0;
-
-      /* On lie la socket à l'adresse qu'on vient de configurer avec bind();  */
-      if(	bind(
-      socket_ecoute,
-      (struct sockaddr *) &adresse_ecoute,
-      sizeof(adresse_ecoute)
-        ) == -1)
-      {
-      perror("bind()");
-      exit(-1);
-      }
-
-      /* On dit au système que cette socket est "passive", qu'elle écoute.
-       * Trois clients au max seront en attente de connexion.
-       * S'il y en a un 4e, il sera rejete				*/
-      if( listen( socket_ecoute, 3) == -1 ){
-      perror( "listen()" );
-      exit(-1);
-    }
-    return socket_ecoute;
-    }
-
-    void serveur1()
-    {
-      int socket_ecoute = connection();
-      int			socket_client[10];			/* Socket pour traiter le client */
-      int max = socket_ecoute;
-      struct sockaddr_in	adresse_client;			/* Adresse du client connecte */
-      char		buffer_adresse[128];		/* Buffer pour l'adresse du client */
-      socklen_t		taille;				/* Taille pour accept() */
-      ssize_t		taille_recue;			/* Quantite d'octets reçue */
-      			/* Buffer pour mettre le message reçu et l'afficher */
-      int taille_envoyee;
-      fd_set rdfs;
-      taille = sizeof( struct sockaddr_in );
-      char buffer_message[128];
-      int i = 0;
-      int nbrCLIENT = 0;
-      int csock;
-      int c;
-      int client;
-
-      /* On traite nos clients dans une boucle infinie */
-      while( 1 ){
-
-        FD_ZERO(&rdfs);
-        FD_SET(STDIN_FILENO, &rdfs);
-        FD_SET(socket_ecoute,&rdfs);
-        for ( i = 0; i < nbrCLIENT; i++)
-        {
-          FD_SET(socket_client[i],&rdfs);
-        }
-        if (select(max + 1,&rdfs,NULL,NULL,NULL ) == -1)
-        {
-          perror("select()");
-          exit(-1);
-        }
-        /*
-        if (FD_ISSET(STDIN_FILENO, &rdfs))
-        {
-          break;
-        }*/
-         if (FD_ISSET(socket_ecoute, &rdfs))
-        {
-          struct sockaddr_in csin = {0};
-          socklen_t taille1 = sizeof(csin);
-         csock =  accept(    socket_ecoute
-            ,   (struct sockaddr *) &adresse_client
-            ,   &taille1);
-            if ( csock== -1 ){
-                perror( "accept()" );
-                exit( -1 );
-            }
-
-        //max = csock > max ? csock : max;
-        FD_SET(csock, &rdfs);
-        c =  csock ;
-        socket_client[nbrCLIENT] = c;
-        nbrCLIENT++;
-      }
-      else
-      {
-        for ( i = 0; i < nbrCLIENT; i++)
-        {
-        if (FD_ISSET(socket_ecoute, &rdfs))
-        {
-          client = socket_client[i];
-          int taille_recue = lire_client(socket_client[i],buffer_message);
-          renvoyer_client(socket_client,buffer_message,nbrCLIENT);
-        }
-        }
-      }
-      printf(	    "Un client s'est connecté ... %s : %d\n"
-           ,   inet_ntop(	AF_INET
-               ,	&(adresse_client.sin_addr)
-               ,	buffer_adresse
-               ,	128 )
-           , adresse_client.sin_port);
-// /*
-//   /* On prend un nouveau client. Cela nous cree une nouvelle socket,
-//    * juste pour lui. Au passage, on atrappe son adresse		*/
-//   printf(	    "Un client s'est connecté ... %s : %d\n"
-//     ,   inet_ntop(	AF_INET
-//         ,	&(adresse_client.sin_addr)
-//         ,	buffer_adresse
-//         ,	128 )
-//     , adresse_client.sin_port);
-//
-//         /*  Je pense qu'il faudrait faire une fonction chat() qui tourne en boucle
-//             Cette fonction serait appelée par un thread.
-//             De cette manière, elle serait gérée indépendament des autres chat().
-//             Ce serait donc un moyen d'avoir des chats simultanés.
-//             Les threads étant avec des variables communes, aucun souci pour transferer
-//             les messages des autres clients d'un thread à l'autre.                      */
-//
-//
-//
-//   /* C'est plus propre, on ne sait pas à quoi ressemble le message    */
-//   memset( buffer_message, 0, sizeof( buffer_message ) );
-//   taille_recue = recv( socket_client, buffer_message, 128, 0);
-//
-//   if ( taille_recue == -1 ){
-//       perror ("recv()" );
-//       exit( -1 );
-//   }
-//
-//   /* Au pire on affichera n'importe quoi, sans déborder du buffer */
-//   buffer_message[127] = '\0';
-//
-//   printf(  "Message reçu (taille %ld): \n[%s]\n"
-//     , taille_recue
-//     , buffer_message );
-//     taille_envoyee = send( socket_client[i], buffer_message, strlen(buffer_message), 0);
-//     if (taille_envoyee == -1)
-//     {
-//   perror("send()");
-//   exit(-1);
-//     }
-
   }
 }
 
-void deconnection(int sock_client)
+
+int connection ()
 {
-close(sock_client);
+
+    int			socket_ecoute;			/* Socket d'écoute */
+    struct sockaddr_in	adresse_ecoute;			/* Adr locale ou le server ecoute */
+    socklen_t		taille;				/* Taille pour accept() */
+
+
+
+    puts(" -- Server lancé -- \n");
+    /* Creation de la socket; AF_INET	->  IPv4; SOCK_STREAM	->  TPC	*/
+    if( ( socket_ecoute = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1){
+        perror("socket()");
+        exit(-1);
+    }
+
+    /* Initialisation de la structure qui contient l'adresse écoutée.
+    * La fonction memset() remplit une zone avec ce qu'on veut (ici, 0) */
+    memset(&adresse_ecoute, 0, sizeof(adresse_ecoute));
+    adresse_ecoute.sin_family = AF_INET; /* IPv4 */
+
+    /* Le server écoute sur le port 52000, choisi au hasard
+    * Attention, il faut envoyer les 2 octets dans le bon ordre -> htons() */
+    adresse_ecoute.sin_port = htons( 52000 ); /* Pour récupérer un nombre converti: utilser ntohs */
+
+    /* Petit bricolage pour l'adresse: je mets juste 0.
+    * Donc 0x00000000, et ça dans n'importe quel ordre c'est l'IP 0.0.0.0
+    * 0.0.0.0 = "écoute sur n'importe quelle IP de la machine"
+    * Comment ça "N'importe quelle IP de la machine ?????		 */
+    adresse_ecoute.sin_addr.s_addr = 0;
+
+    /* On lie la socket à l'adresse qu'on vient de configurer avec bind();  */
+    if(	bind(
+        socket_ecoute,
+        (struct sockaddr *) &adresse_ecoute,
+        sizeof(adresse_ecoute)
+    ) == -1)
+    {
+        perror("bind()");
+        exit(-1);
+    }
+
+    /* On dit au système que cette socket est "passive", qu'elle écoute.
+    * Trois clients au max seront en attente de connexion.
+    * S'il y en a un 4e, il sera rejete				*/
+    if( listen( socket_ecoute, 3) == -1 ){
+        perror( "listen()" );
+        exit(-1);
+    }
+    return socket_ecoute;
 }
+/* Fonction qui gere les connections / transmission de message */
+void serveur1()
+{
+    int socket_ecoute = connection();// variable qui prend le socket d'ecoute.
+    int			socket_client[10];			/* Socket pour traiter le client */
+    int max = socket_ecoute;
+    struct sockaddr_in	adresse_client;			/* Adresse du client connecte */
+    char		buffer_adresse[128];		/* Buffer pour l'adresse du client */
+    socklen_t		taille;				/* Taille pour accept() */
+    ssize_t		taille_recue;			/* Quantite d'octets reçue */
 
- int main() {
+    int taille_envoyee;
+    fd_set rd;
+    taille = sizeof( struct sockaddr_in );
+    char buffer_message[128];       /* Buffer pour mettre le message reçu et l'afficher */
+    int i = 0;
+    int nbrCLIENT = 0;
+    int socket;
+    int c;
+    int client;
 
-serveur1();
+    /* On traite nos clients dans une boucle infinie */
+    while( 1 ){
+printf("socket ecoude : %d  \n",socket_ecoute);
+        FD_ZERO(&rd);// ecrase le descripteur
+        FD_SET(STDIN_FILENO, &rd);// initialise le descripteur sur une entré clavier
+        FD_SET(socket_ecoute,&rd);// initialise le descripteur avec le socket écoute
+        for ( i = 0; i < nbrCLIENT; i++)// boucle qui gère les client
+        {
+            FD_SET(socket_client[i],&rd);//pour chaque client on initialise le fichier descripteur
+        }
+        if (select(max + 1,&rd,NULL,NULL,NULL ) == -1)//on test l'ensemble des descripteur en lecture
+        {
+            perror("select()");
+            exit(-1);
+        }
 
-  return 0;
+        if (FD_ISSET(STDIN_FILENO, &rd)) //test si on a une entree clavier
+        {
+            break;// on quitte si on appuies sur n'importe quel touche
+        }
+        else if (FD_ISSET(socket_ecoute, &rd))// test si le socket ecoute à changer
+        {
+            struct sockaddr_in adresse_client; // alors on connecte un nouveau client
+            adresse_client.sin_family = AF_INET;
+            adresse_client.sin_port = htons (52000);
+            socklen_t taille1 = sizeof(adresse_client);
+            socket =  accept(    socket_ecoute
+                ,   (struct sockaddr *) &adresse_client
+                ,   &taille1);
+                if ( socket== -1 ){
+                    perror( "accept()" );//accepte renvoie un descripteur
+                    exit( -1 );
+                }
+                  max = socket > max ? socket : max;
+                  printf(	    "Un client s'est connecté ... %s : %d\n"
+                  ,   inet_ntop(	AF_INET
+                      ,	&(adresse_client.sin_addr)
+                      ,	buffer_adresse
+                      ,	128 )
+                      , adresse_client.sin_port);
+
+
+
+                FD_SET(socket, &rd);// on zjoute socket dans rd
+              //  c =  csock ;
+                socket_client[nbrCLIENT] = socket;// le nombre de client est égales on nombre de descripteur dans l'ensemble
+                nbrCLIENT++;
+            }
+            else // si y a pas de demande de connection
+            {
+                for ( i = 0; i < nbrCLIENT; i++)// on boucle tout les clients
+                {
+                    if (FD_ISSET(socket_client[i], &rd))// on verifie les descripteur
+
+                    {
+
+                        int client_parle = socket_client[i];
+                        taille_recue = lire_client(socket_client[i],buffer_message);
+
+
+
+                        //lire_et_renvoyer_client(socket_client,client,buffer_message,nbrCLIENT);
+
+                        //else {perror(recv());}
+
+                        for ( i = 0; i < nbrCLIENT; i++)
+                        {
+                          if (socket_client[i] != client_parle)
+                          {
+                          envoyer_client(socket_client[i],buffer_message);
+                          }
+                      }
+                    }
+                }
+            }
+
+                // /*
+                //   /* On prend un nouveau client. Cela nous cree une nouvelle socket,
+                //    * juste pour lui. Au passage, on atrappe son adresse		*/
+                //   printf(	    "Un client s'est connecté ... %s : %d\n"
+                //     ,   inet_ntop(	AF_INET
+                //         ,	&(adresse_client.sin_addr)
+                //         ,	buffer_adresse
+                //         ,	128 )
+                //     , adresse_client.sin_port);
+                //
+                //         /*  Je pense qu'il faudrait faire une fonction chat() qui tourne en boucle
+                //             Cette fonction serait appelée par un thread.
+                //             De cette manière, elle serait gérée indépendament des autres chat().
+                //             Ce serait donc un moyen d'avoir des chats simultanés.
+                //             Les threads étant avec des variables communes, aucun souci pour transferer
+                //             les messages des autres clients d'un thread à l'autre.                      */
+                //
+                //
+                //
+                //   /* C'est plus propre, on ne sait pas à quoi ressemble le message    */
+                //   memset( buffer_message, 0, sizeof( buffer_message ) );
+                //   taille_recue = recv( socket_client, buffer_message, 128, 0);
+                //
+                //   if ( taille_recue == -1 ){
+                //       perror ("recv()" );
+                //       exit( -1 );
+                //   }
+                //
+                //   /* Au pire on affichera n'importe quoi, sans déborder du buffer */
+                //   buffer_message[127] = '\0';
+                //
+                //   printf(  "Message reçu (taille %ld): \n[%s]\n"
+                //     , taille_recue
+                //     , buffer_message );
+                //     taille_envoyee = send( socket_client[i], buffer_message, strlen(buffer_message), 0);
+                //     if (taille_envoyee == -1)
+                //     {
+                //   perror("send()");
+                //   exit(-1);
+                //     }
+
+            }
+        }
+
+        void deconnection(int sock_client)
+        {
+            close(sock_client);
+        }
+
+int main() {
+    serveur1();
+    return 0;
 }
